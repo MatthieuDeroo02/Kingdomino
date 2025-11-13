37925ea2b42010c68bd57e68e445ea8f2374124f
PC <matthieu.deroo@etu.univ-smb.fr>
2025-11-13 18:31:18 +0100

    ajout des boutons choix du nbr de joueur

--- src/render.c
+++ src/render.c
@@ -55,7 +55,12 @@ SDL_Texture *Render_LoadAndConvertImage(const char *cheminImage, SDL_Window *win
 
 void Render_AfficherLobby(SDL_Window *window) {
     SDL_Renderer *renderer = Render_CreatRenderer(window); //Crée le moteur graphique du lobby
-    SDL_Texture *texture = Render_LoadAndConvertImage("assets/images/fond_ecran_lobby2.bmp", window, renderer);
+    SDL_Texture *fond = Render_LoadAndConvertImage("assets/images/fond_ecran_lobby2.bmp", window, renderer); //Crée la texture du fond d'ecran
+
+    T_Button Button2Player = Render_AddButton(renderer, "assets/images/Button2player.png", 350, 510, 0, 0); //Importe les Buttons utilisé lors
+    T_Button Button3Player = Render_AddButton(renderer, "assets/images/Button3player.png", 350, 510, 0, 0);
+    T_Button Button4Player = Render_AddButton(renderer, "assets/images/Button4player.png", 350, 510, 0, 0);
+
 
     int running = 1;
     while (running) {
@@ -66,12 +71,46 @@ void Render_AfficherLobby(SDL_Window *window) {
             }
         }
 
+        int largeur, hauteur;
+        SDL_GetRenderOutputSize(renderer, &largeur, &hauteur);
+
+
+        //SDL_FRect Button2Player ={ 0.281*largeur, 0.417*hauteur, 0.117*largeur, 0.303*hauteur};
+        //SDL_FRect Button3Player ={ 0.441*largeur, 0.417*hauteur, 0.117*largeur, 0.303*hauteur};
+        //SDL_FRect Button4Player ={ 0.602*largeur, 0.417*hauteur, 0.117*largeur, 0.303*hauteur};
+
+
         SDL_RenderClear(renderer);
-        SDL_RenderTexture(renderer, texture, NULL, NULL);  // dessine l'image en plein écran
+        SDL_RenderTexture(renderer, fond, NULL, NULL);  // dessine l'image en plein écran
+        SDL_RenderTexture(renderer, Button2Player.texture, NULL, &Button2Player.rectangle);
+        SDL_RenderTexture(renderer, Button3Player.texture, NULL, &Button3Player.rectangle);
+        SDL_RenderTexture(renderer, Button4Player.texture, NULL, &Button4Player.rectangle);
         SDL_RenderPresent(renderer);
         SDL_Delay(16);  // ~60 FPS
     }
 
-    SDL_DestroyTexture(texture);
+    SDL_DestroyTexture(fond);
     SDL_DestroyRenderer(renderer);
+}
+
+
+T_Button Render_AddButton(SDL_Renderer *renderer, const char *cheminImage, float sizeX, float sizeY, float positionX, float positionY) {
+    T_Button Button;
+    Button.overhead = 0;
+    Button.cheminImage = cheminImage;
+    Button.rectangle.x = sizeX;
+    Button.rectangle.y = sizeY;
+    Button.rectangle.w = positionX;
+    Button.rectangle.h = positionY;
+    Button.texture = IMG_LoadTexture(renderer, cheminImage);
+    return Button;
+}
+
+bool Render_ButtonOverhead(T_Button button) {
+    float mouseX, mouseY;
+    SDL_GetMouseState(&mouseX, &mouseY);
+    if (button.rectangle.x <= mouseX && mouseX <= button.rectangle.x + button.rectangle.w && button.rectangle.y <= mouseX && mouseY <= button.rectangle.y + button.rectangle.h) {
+        return true;
+    }
+    return false;
 }
--- src/render.h
+++ src/render.h
@@ -2,8 +2,17 @@
 #define RENDER_H
 
 #include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
 #include <stdlib.h>
 #include "game.h"
+
+typedef struct{
+    const char *cheminImage;
+    bool overhead;
+    SDL_FRect rectangle;
+    SDL_Texture *texture;
+} T_Button;
+
 
 /*Initialise SDL video*/
 void Render_InitVideo();
@@ -14,10 +23,12 @@ SDL_Renderer *Render_CreatRenderer(SDL_Window *window);
 /*Charge et compile une image*/
 SDL_Texture *Render_LoadAndConvertImage(const char *cheminImage, SDL_Window *window, SDL_Renderer *renderer);
 
-
-
 /*affiche le lobby*/
 void Render_AfficherLobby(SDL_Window *window);
 
+/*Crée un Button qui pouras etre utilisé plus tard*/
+T_Button Render_AddButton(SDL_Renderer *renderer, const char *cheminImage, float sizeX, float sizeY, float positionX, float positionY);
+/*Returne true si le Button est survolé*/
+bool Render_ButtonOverhead(T_Button button);
 
 #endif
