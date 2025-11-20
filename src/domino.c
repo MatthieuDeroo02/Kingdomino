168520cdb4252dfd94596b2e8bfc395b4ffd1679
PC <matthieu.deroo@etu.univ-smb.fr>
2025-11-20 14:15:25 +0100

    correctif de bug

--- src/domino.c
+++ src/domino.c
@@ -1,6 +1,6 @@
 #include "domino.h"
 
 T_Domino *Domino_GenerateDomino() {
-    T_Domino *Domino = malloc(nbrDomino * sizeof(T_Domino));
-    Domino[0]={}
+    T_Domino *domino = malloc(nbrDomino * sizeof(T_Domino));
+    return domino;
 }
